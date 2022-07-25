b2e7063472b2aad21b955ce75daa3ce55cbb895d
James Woolfenden <james.woolfenden@gmail.com>
2022-07-25 23:00:00 +0100

    cleanup

--- example/examplea/provider.aws.tf
+++ example/examplea/provider.aws.tf
@@ -1,8 +1,10 @@
 provider "aws" {
   region = "eu-west-2"
   default_tags {
-    application = "terraform"
-    module      = "terraform-aws-statebucket"
-    createdby   = "James Woolfenden"
+    tags = {
+      application = "terraform"
+      module      = "terraform-aws-statebucket"
+      createdby   = "James Woolfenden"
+    }
   }
 }
