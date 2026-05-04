0642a0cbb1f87e70ca0d5a73998daf65b1020390
James Woolfenden <james.woolfenden@gmail.com>
2026-05-04 15:25:16 +0100

    fix: skip CKV2_AWS_61 and CKV2_AWS_62 on S3 bucket resources

--- aws_s3_bucket.state_bucket.tf
+++ aws_s3_bucket.state_bucket.tf
@@ -1,4 +1,6 @@
 resource "aws_s3_bucket" "state_bucket" {
+  # checkov:skip=CKV2_AWS_61: Lifecycle configuration not required for this bucket
+  # checkov:skip=CKV2_AWS_62: Event notifications not required for this bucket
   # checkov:skip=CKV_AWS_144: ADD REASON
   # checkov:skip=CKV_AWS_21:Legacy
   # checkov:skip=CKV_AWS_18: "Ensure the S3 bucket has access logging enabled"
